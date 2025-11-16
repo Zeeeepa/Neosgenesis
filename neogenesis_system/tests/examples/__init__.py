80c3b68cb6c7f12ab84d5ad42b9435c01848baa8
answeryt <answeryt@qq.com>
2025-11-16 15:30:34 +0800

    Delete neogenesis_system directory

--- neogenesis_system/tests/examples/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-示例测试模块
-Example Test Module
-"""
--- neogenesis_system/tests/unit/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-单元测试模块
-Unit Test Module
-"""
